b3eff773c3f91d23613a2c07c9cc6503f9e888b7
Asad ali <46523348+AsadAli00@users.noreply.github.com>
2020-06-26 22:35:37 +0500

    Update room.js
    create two usestate react function and applying in button to action

--- src/room.js
+++ src/room.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function Room() {
-  return (
+    let [isLit,setLit] = useState(true);
+    let [age,setAge] = useState(23);
+
+    // function UpdateLit(){
+    //     console.log("Button clicked");
+    //     //isLit = !isLit;
+    //     setLit(!isLit);
+    //  }
+    // function IncreaseAge(){
+    //     console.log("Increase age")
+    //     setAge(++age);
+    // }
+
+    // const IncreaseAge = () =>{
+    //     console.log("Increase age")
+    //     setAge(++age);
+    // }
+  
+    return (
     <div>
-      This is Room Component
+      This Room is {isLit? 'Lit': "Dark"}
+      <br/>
+      age = {age}
+      <br/>
+      <button onClick={() => setLit(!isLit)} >Toggle Light</button>
+      <br/>
+      <button onClick={() =>{
+                        console.log("Increase age arrow function")
+                        setAge(++age);
+                      }} 
+                      >Increase Age</button>
     </div>
   );
     
